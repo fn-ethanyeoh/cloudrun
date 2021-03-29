37ba4a348c61b07227c3432c49de6ae4762a901a
Ishaan Ohri <ishaan99ohri@gmail.com>
2021-03-29 23:18:48 +0530

    dev script working

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+const PORT: number = Number(process.env.PORT) || 3000;
+const HOST: string = String(process.env.HOST);
+
+app.use('/', (req: Request, res: Response) => {
+    res.send('Hello from Node.js Server');
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`🚀 Server running on http://${HOST}:${PORT} 🚀`);
+})
